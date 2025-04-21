d20fc979064402d7ffd9b94c1d981bf897754b86
athageo <147268689+athageo@users.noreply.github.com>
2025-04-21 23:13:08 +0000

    Refactor populate_db command to streamline data population for users, teams, activities, leaderboard, and workouts, enhancing clarity and efficiency

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,41 +1,62 @@
 from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
 from octofit_tracker.test_data import test_users, test_teams, test_activities, test_leaderboard, test_workouts
-from datetime import timedelta
+from bson import ObjectId
 
 class Command(BaseCommand):
-    help = 'Populate the database with test data'
+    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
 
     def handle(self, *args, **kwargs):
+        # Clear existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
         # Populate users
         for user_data in test_users:
-            user, created = User.objects.get_or_create(**user_data)
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'User created: {user.username}'))
+            user = User.objects.create(
+                _id=user_data['_id'],
+                username=user_data['username'],
+                email=user_data['email'],
+                password=user_data['password']
+            )
+            self.stdout.write(self.style.SUCCESS(f'User created: {user.username}'))
 
         # Populate teams
         for team_data in test_teams:
-            members = team_data.pop('members')
-            team, created = Team.objects.get_or_create(**team_data)
-            if created:
-                team.members = list(User.objects.filter(username__in=members))
-                team.save()
-                self.stdout.write(self.style.SUCCESS(f'Team created: {team.name}'))
+            team = Team.objects.create(
+                _id=team_data['_id'],
+                name=team_data['name']
+            )
+            team.members.set(User.objects.filter(_id__in=team_data['members']))
+            self.stdout.write(self.style.SUCCESS(f'Team created: {team.name}'))
 
         # Populate activities
         for activity_data in test_activities:
-            activity, created = Activity.objects.get_or_create(**activity_data)
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'Activity created: {activity.activity_type} for user {activity.user.username}'))
+            activity = Activity.objects.create(
+                _id=activity_data['_id'],
+                user=User.objects.get(_id=activity_data['user']),
+                activity_type=activity_data['activity_type'],
+                duration=activity_data['duration']
+            )
+            self.stdout.write(self.style.SUCCESS(f'Activity created: {activity.activity_type} for user {activity.user.username}'))
 
         # Populate leaderboard
         for leaderboard_data in test_leaderboard:
-            leaderboard, created = Leaderboard.objects.get_or_create(**leaderboard_data)
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'Leaderboard entry created for user {leaderboard.user.username} with score {leaderboard.score}'))
+            leaderboard = Leaderboard.objects.create(
+                _id=leaderboard_data['_id'],
+                user=User.objects.get(_id=leaderboard_data['user']),
+                score=leaderboard_data['score']
+            )
+            self.stdout.write(self.style.SUCCESS(f'Leaderboard entry created for user {leaderboard.user.username} with score {leaderboard.score}'))
 
         # Populate workouts
         for workout_data in test_workouts:
-            workout, created = Workout.objects.get_or_create(**workout_data)
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'Workout created: {workout.name}'))
+            workout = Workout.objects.create(
+                _id=workout_data['_id'],
+                name=workout_data['name'],
+                description=workout_data['description']
+            )
+            self.stdout.write(self.style.SUCCESS(f'Workout created: {workout.name}'))
